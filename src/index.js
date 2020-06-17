0bf0682a13fcc75be0051903c9991a4db29413ee
nrcfrld <nrcfrld@gmail.com>
2020-06-18 05:16:35 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// You can specify which plugins you need
+import "bootstrap";
+import "./scss/main.scss";
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[contentHash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
