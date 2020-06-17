65549bda72611246a2ae97f2c382ac654b1126f3
nrcfrld <nrcfrld@gmail.com>
2020-06-18 06:59:36 +0700

    add clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -3,17 +3,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif|jpeg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "images",
+          },
+        },
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const common = require("./webpack.common.js");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [new CleanWebpackPlugin()],
   output: {
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist"),
